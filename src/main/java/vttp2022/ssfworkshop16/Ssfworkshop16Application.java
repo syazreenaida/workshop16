e80b24ec418fa47a2faa6bcb8b3864898a23529f
brendontwj <bren0019@e.ntu.edu.sg>
2022-07-26 13:49:12 +0800

    init

---
+++ src/main/java/vttp2022/ssfworkshop16/Ssfworkshop16Application.java
@@ -0,0 +1,13 @@
+package vttp2022.ssfworkshop16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssfworkshop16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssfworkshop16Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssfworkshop16/Ssfworkshop16ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssfworkshop16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssfworkshop16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
